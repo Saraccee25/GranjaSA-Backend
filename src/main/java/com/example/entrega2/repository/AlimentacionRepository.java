5aca290de1125968933686f9aafa4f1afa3f6304
Sara Castañeda <saracastanedaecheverri@gmail.com>
2025-09-05 15:43:05 -0500

    Add repositories

---
+++ src/main/java/com/example/entrega2/repository/AlimentacionRepository.java
@@ -0,0 +1,7 @@
+package com.example.entrega2.repository;
+
+import com.example.entrega2.entity.Alimentacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlimentacionRepository extends JpaRepository<Alimentacion, Long> {
+}
---
+++ src/main/java/com/example/entrega2/repository/PorcinoRepository.java
@@ -0,0 +1,7 @@
+package com.example.entrega2.repository;
+
+import com.example.entrega2.entity.Porcino;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PorcinoRepository extends JpaRepository<Porcino, Long> {
+}
